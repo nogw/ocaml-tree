ff7dc6c3a8c4de4c93de92d932e648ea8d9e8ce1
nogw <gabrielnogueiraoliveira@gmail.com>
2021-11-04 02:12:10 +0000

    print all files and subdirectories

--- lib/utils.ml
+++ lib/utils.ml
@@ -5,11 +5,11 @@ let list_root_files dir = Sys.readdir dir
 
 let rec print_dir path dir =
   let is_dir file =
-    print_endline file;
-    match Sys.is_directory file with
+    print_endline (path ^ "/" ^ file);
+    match Sys.is_directory (path ^ "/" ^ file) with
     | true ->
       print_dir (path ^ "/" ^ file) (list_root_files (path ^ "/" ^ file) |> Array.to_list)
-    | false -> print_endline file
+    | false -> print_endline (path ^ "/" ^ file)
   in
   match dir with
   | [] -> failwith "empty directory"
