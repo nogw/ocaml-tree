beda78d923ed3de55a5ae244aa8350e8425d52e2
nogw <gabrielnogueiraoliveira@gmail.com>
2021-11-05 02:31:00 +0000

    update gitignore

--- _build/default/bin/main.ml
+++
@@ -1,8 +0,0 @@
-open Lib
-(* |> [%to_yojson: string array]
-|> Yojson.Safe.to_string *)
-
-let () =
-  let dir = Utils.directory in
-  dir |> Utils.list_root_files |> Array.to_list |> Utils.print_dir dir
-;;
