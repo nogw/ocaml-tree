72d669bc4073c9623fb96b15e628caa581fdb16e
nogw <gabrielnogueiraoliveira@gmail.com>
2021-11-07 02:17:28 +0000

    flag to pass a path instead of the current directory

--- bin/tree.ml
+++ bin/tree.ml
@@ -1,17 +1,26 @@
 open Lib
 open Cmdliner
 
+let path =
+  let info = Arg.info [ "p"; "P"; "path" ] ~docv:"PATH" ~doc:"path to directory" in
+  Arg.value (Arg.opt (Arg.some Arg.dir) None info)
+;;
+
 let color =
   let info = Arg.info [ "c"; "C"; "color" ] ~doc:"print with colors" in
   Arg.value (Arg.flag info)
 ;;
 
 let cmdliner_term =
-  let dir = Utils.directory in
-  let tree f =
-    dir |> Utils.list_root_files |> Array.to_list |> Utils.folder_tree dir ~colors:f
+  let tree f p =
+    let path =
+      match p with
+      | None -> Utils.directory
+      | Some p -> p
+    in
+    path |> Utils.list_root_files |> Array.to_list |> Utils.folder_tree path ~colors:f
   in
-  Term.(const tree $ color)
+  Term.(const tree $ color $ path)
 ;;
 
 let () =
