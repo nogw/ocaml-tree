e80e73d57661e69f0a7724b4fdb2ef6a0d9ee3ce
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-14 09:11:13 -0300

    remove unused function

--- lib/utils.ml
+++ lib/utils.ml
@@ -3,26 +3,21 @@ let white_bold = Fmt.(styled `White (styled `Bold string))
 let directory = Sys.getcwd ()
 let list_root_files dir = Sys.readdir dir
 
-let rec first_last = function
-  | [] -> "", ""
-  | [ e1 ] -> "", e1
-  | [ e1; e2 ] -> e1, e2
-  | e1 :: _ :: r -> first_last (e1 :: r)
-;;
-
 let print_file_or_folder i s f t c =
   match t with
   | `Folder ->
-    if c = true then Fmt.pr "%s%s %a\n" i s white_bold f else Fmt.pr "%s%s %s\n" i s f
-  | `File -> if c = true then Fmt.pr "%s%s %a\n" i s green f else Fmt.pr "%s%s %s\n" i s f
+    if c then Fmt.pr "%s%s %a\n" i s white_bold f else Fmt.pr "%s%s %s\n" i s f
+  | `File -> if c then Fmt.pr "%s%s %a\n" i s green f else Fmt.pr "%s%s %s\n" i s f
 ;;
 
 let rec folder_tree path ?(ident = "") ?(colors = false) ?(ignore = []) dir =
   let is_dir file =
-    let is_last = snd (first_last dir) = file in
+    let is_last = List.nth dir (pred(List.length dir)) = file in
     let symbol = if is_last then "└──" else "├──"
     and ident' = if is_last then "    " else "│   "
+
     and path' = path ^ "/" ^ file in
+    
     match Sys.is_directory path' with
     | true ->
       print_file_or_folder ident symbol file `Folder colors;
